62a3b25e3a97fed3312b8fe36a798a51fdbba0a6
PJMantoss <joelptoss@gmail.com>
2021-08-02 12:41:05 +0100

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
-  return <Router>
+  return( 
+  <Router>
+    <MainNavigation />
     <Switch>
       <Route path="/" exact>
         <Users />
@@ -16,7 +19,7 @@ function App() {
 
       <Redirect to="/" exact />
     </Switch>
-  </Router>;
+  </Router>);
 }
 
 export default App;
