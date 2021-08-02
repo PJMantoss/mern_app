c4ff31e1b139f9c83f492d85c9e9dc9228d69129
PJMantoss <joelptoss@gmail.com>
2021-08-02 11:50:23 +0100

    update UserItem

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -10,16 +10,16 @@ const UserItem = props => {
         <li className="user-item">
             <Card className="user-item__content">
                 <Link to={`/${props.id}/places`} >
-                <div className="user-item__image">
-                    <Avatar image={props.image} alt={props.alt} />
-                </div>
+                    <div className="user-item__image">
+                        <Avatar image={props.image} alt={props.alt} />
+                    </div>
 
-                <div className="user-item__info">
-                    <h2>{props.name}</h2>
-                    <h3>
-                        {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
-                    </h3>
-                </div>
+                    <div className="user-item__info">
+                        <h2>{props.name}</h2>
+                        <h3>
+                            {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+                        </h3>
+                    </div>
                 </Link>
             </Card>
         </li>
