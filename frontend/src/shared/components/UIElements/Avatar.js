d8eeb90d54bb42ac8e7586e5747630d26c21e0e2
PJMantoss <joelptoss@gmail.com>
2021-08-02 09:18:13 +0100

    avatar styling

---
+++ frontend/src/shared/components/UIElements/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Avatar.css';
+
+const Avatar = () => {
+    return (
+        <div>
+            <img />
+        </div>
+    )
+}
+
+export default Avatar;
---
+++ frontend/src/user/components/UsersList.js
@@ -0,0 +1,30 @@
+import React, { useLayoutEffect } from 'react';
+import UserItem from './UserItem';
+
+import './UserItem.css'
+
+const UsersList = props => {
+    if(props.users.length === 0){
+        return (
+            <div className="center">
+                <h2>No users found.</h2>
+            </div>
+        )
+    }
+
+    return (
+        <ul>
+            {props.users.map(user => (
+                <UserItem 
+                    key={user.id} 
+                    id={user.id} 
+                    image={user.image}
+                    name={user.name}
+                    placeCount={user.places}
+                />
+            ))}
+        </ul>
+    )
+};
+
+export default UsersList;
