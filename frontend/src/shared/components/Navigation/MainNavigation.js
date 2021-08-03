e75ebfea217affff49d3e8cc95e75e7f7094bb82
PJMantoss <joelptoss@gmail.com>
2021-08-03 09:37:12 +0100

    update MainNavigation

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -8,19 +8,26 @@ import './MainNavigation.css';
 
 const MainNavigation = props => {
     return(
-        <MainHeader>
-            <button className="main-navigation__menu-btn">
-                <span />
-                <span />
-                <span />
-            </button>
-            <h1 className="main-navigation__title">
-                <Link to="/" >Places</Link>
-            </h1>
-            <nav className="main-navigation__header-nav">
-                <NavLinks />
-            </nav>
-        </MainHeader>
+        <>
+            <SideDrawer>
+                <nav>
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
+            <MainHeader>
+                <button className="main-navigation__menu-btn">
+                    <span />
+                    <span />
+                    <span />
+                </button>
+                <h1 className="main-navigation__title">
+                    <Link to="/" >Places</Link>
+                </h1>
+                <nav className="main-navigation__header-nav">
+                    <NavLinks />
+                </nav>
+            </MainHeader>
+        </>
     )
 };
 
