a271aae7e828e468a61dbe90f812451517002e74
PJMantoss <joelptoss@gmail.com>
2021-08-03 09:28:09 +0100

    update SideDrawer

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import './SideDrawer.css'
+
 const SideDrawer = props => {
     return(
         <aside>{props.children}</aside>
     )
 }
+
+export default SideDrawer;
