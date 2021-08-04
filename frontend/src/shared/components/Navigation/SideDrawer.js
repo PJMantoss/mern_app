e7808af6aaf40c51270ab940c908898ff14198db
PJMantoss <joelptoss@gmail.com>
2021-08-04 19:21:29 +0100

    update index.html

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -3,9 +3,8 @@ import React from 'react';
 import './SideDrawer.css'
 
 const SideDrawer = props => {
-    return(
-        <aside className="side-drawer">{props.children}</aside>
-    )
+    const content = <aside className="side-drawer">{props.children}</aside>;
+    return
 }
 
 export default SideDrawer;
