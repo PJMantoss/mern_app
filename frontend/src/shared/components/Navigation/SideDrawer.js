8157a2d39ac274fc352c8d577573e1753a25b804
PJMantoss <joelptoss@gmail.com>
2021-08-04 20:01:00 +0100

    installed ReactTransitions

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -5,7 +5,11 @@ import { CSSTransition } from 'react-transition-group';
 import './SideDrawer.css'
 
 const SideDrawer = props => {
-    const content = <aside className="side-drawer">{props.children}</aside>;
+    const content = (
+        <CSSTransition>
+            <aside className="side-drawer">{props.children}</aside>
+        </CSSTransition>
+    );
 
     return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
 }
