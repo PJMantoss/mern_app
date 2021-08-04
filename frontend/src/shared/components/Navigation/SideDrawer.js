5d730caabb5371976c85b6d4d15dc9e322bd1225
PJMantoss <joelptoss@gmail.com>
2021-08-04 19:22:59 +0100

    update SideDrawer

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -4,7 +4,8 @@ import './SideDrawer.css'
 
 const SideDrawer = props => {
     const content = <aside className="side-drawer">{props.children}</aside>;
-    return
+
+    return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
 }
 
 export default SideDrawer;
