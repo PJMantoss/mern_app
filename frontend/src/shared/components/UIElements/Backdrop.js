eae912e3a266e8b2e94d56e930ce1840f16bd9c3
PJMantoss <joelptoss@gmail.com>
2021-08-04 19:40:41 +0100

    update Backdrop

--- frontend/src/shared/components/UIElements/Backdrop.js
+++ frontend/src/shared/components/UIElements/Backdrop.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Backdrop = () => {}
+const Backdrop = () => {
+    return ReactDOM.createPortal(
+        <div></div>, document.getElementById('backdrop-hook');
+    )
+}
 
 export default Backdrop;
