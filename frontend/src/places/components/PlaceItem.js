3528189eba8d5fbc18f7734e4a4db48642554dcd
PJMantoss <joelptoss@gmail.com>
2021-08-09 19:47:34 +0100

    update PlaceItem

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -13,25 +13,37 @@ const PlaceItem = props => {
     const closeMap = () => setShowMap(false);
 
     return(
-        <Card className="place-item__content">
-            <li className="place-item">
-                <div className="place-item__image">
-                    <img src={props.image} alt={props.title} />
-                </div>
+        <React.Fragment>
+            <Modal
+                show={}
+                onCancel={}
+                header={}
+                contentClass=""
+                footerClass=""
+                footer={}
+            >
+                <div className="map-container"></div>
+            </Modal>
+            <Card className="place-item__content">
+                <li className="place-item">
+                    <div className="place-item__image">
+                        <img src={props.image} alt={props.title} />
+                    </div>
 
-                <div className="place-item__info">
-                    <h2>{props.title}</h2>
-                    <h3>{props.address}</h3>
-                    <p>{props.description}</p>
-                </div>
+                    <div className="place-item__info">
+                        <h2>{props.title}</h2>
+                        <h3>{props.address}</h3>
+                        <p>{props.description}</p>
+                    </div>
 
-                <div className="place-item__actions">
-                    <Button inverse>VIEW ON MAP</Button>
-                    <Button to={`places/${props.id}`}>EDIT</Button>
-                    <Button danger>DELETE</Button>
-                </div>
-            </li>
-        </Card>
+                    <div className="place-item__actions">
+                        <Button inverse>VIEW ON MAP</Button>
+                        <Button to={`places/${props.id}`}>EDIT</Button>
+                        <Button danger>DELETE</Button>
+                    </div>
+                </li>
+            </Card>
+        </React.Fragment>
     )
 } 
 
