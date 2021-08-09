2efe4c07f3b3de21f1dbd4c47a09a64affa833b9
PJMantoss <joelptoss@gmail.com>
2021-08-09 20:28:28 +0100

    update PlaceItem

--- frontend/src/shared/components/UIElements/Modal.js
+++ frontend/src/shared/components/UIElements/Modal.js
@@ -11,7 +11,7 @@ const ModalOverlay = props => {
             <header className={`modal__header ${props.headerClass}`}>
                 <h2>{props.header}</h2>
             </header>
-            <form onSubmit={props.onSubmit ? props.onSumit : e => e.preventDefault}>
+            <form onSubmit={props.onSubmit ? props.onSubmit : e => e.preventDefault()}>
                 <div className={`modal__content ${props.contentClass}`}>
                     {props.children}
                 </div>
