919ad62d58e6d4b8c07d7a76377bf22df639b57b
PJMantoss <joelptoss@gmail.com>
2021-08-09 11:00:51 +0100

    update UserPlaces

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -11,7 +11,21 @@ const DUMMY_PLACES = [
         latitude: {
             lat: 40.7484,
             lng: 73.9857
-        }
+        },
+        creator: 'u1'
+    },
+
+    {
+        id: 'p2',
+        title: "Empire State Building",
+        description: 'One of the most famous skycrapers in the world',
+        imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
+        address: '20 W 34th St, New York, NY 10001, USA',
+        latitude: {
+            lat: 40.7484,
+            lng: 73.9857
+        },
+        creator: 'u2'
     }
 ]
 
