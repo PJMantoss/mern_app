9d39ed5df5cf0b07cf5f889400466673ad32ed64
PJMantoss <joelptoss@gmail.com>
2021-08-09 15:41:57 +0100

    update UserPlaces

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -32,9 +32,9 @@ const DUMMY_PLACES = [
 
 const UserPlaces = () => {
     const userId = useParams().userId;
-    const loadedPlaces = DUMMY_PLACES.filter();
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
     return(
-        <PlaceList items={DUMMY_PLACES} />
+        <PlaceList items={loadedPlaces} />
     )
 } 
 
