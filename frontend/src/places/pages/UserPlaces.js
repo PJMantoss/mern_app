e59f39c91222e1fcb521ee621c8a8939d6b0946b
PJMantoss <joelptoss@gmail.com>
2021-08-09 10:54:51 +0100

    update UserPlaces

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: "Empire State Building",
+        description: 'One of the most famous skycrapers in the world',
+        imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
+        address: '',
+        coordinates: ''
+    }
+]
 
 const UserPlaces = () => {
-    return
+    return(
+        <PlaceList />
+    )
 } 
 
 export default UserPlaces;
