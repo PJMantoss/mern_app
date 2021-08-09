53e8505ff137aa154e3ee2ce6a2b8226c873e01e
PJMantoss <joelptoss@gmail.com>
2021-08-09 10:36:05 +0100

    update PlaceItem

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './PlaceItem.css';
 
-const PlaceItem = () => {
+const PlaceItem = props => {
     return(
         <li className="place-item">
-            <div className="place-item__image"></div>
+            <div className="place-item__image">
+                <img src={props.image} alt={props.title} />
+            </div>
 
             <div className="place-item__info"></div>
         </li>
