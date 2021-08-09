3cc2b15973960c0268331408a22d00bb699c5ff7
PJMantoss <joelptoss@gmail.com>
2021-08-09 10:34:01 +0100

    update PlaceItem

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './PlaceItem.css';
 
 const PlaceItem = () => {
-    return
+    return(
+        <li className="place-item">
+            
+        </li>
+    )
 } 
 
 export default PlaceItem;
