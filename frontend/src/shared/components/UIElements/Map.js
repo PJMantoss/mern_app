6527066ec81ab35460adca116bde423dd047a212
PJMantoss <joelptoss@gmail.com>
2021-08-15 20:34:39 +0100

    update Map component

--- frontend/src/shared/components/UIElements/Map.js
+++ frontend/src/shared/components/UIElements/Map.js
@@ -1,14 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 const Map = props => {
     const mapRef = useRef();
 
-    const map = window.google.maps.Map(mapRef.current, {
-        center: props.center,
-        zoom: props.zoom
-    });
-
-    new window.google.maps.Marker({ position: props.center, map: map })
+    useEffect(() => {
+        const map = window.google.maps.Map(mapRef.current, {
+            center: props.center,
+            zoom: props.zoom
+        });
+    
+        new window.google.maps.Marker({ position: props.center, map: map });
+    }, [])
 
     return(
         <div ref={mapRef} className={`map ${props.className}`} style={props.style}></div>
