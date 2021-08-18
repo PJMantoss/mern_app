568efd75f0e9f837051d3ce2f17e4ed69347cf64
PJMantoss <joelptoss@gmail.com>
2021-08-18 14:36:47 +0100

    update UserPlaces

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -9,7 +9,7 @@ const DUMMY_PLACES = [
         description: 'One of the most famous skycrapers in the world',
         imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
         address: '20 W 34th St, New York, NY 10001, USA',
-        latitude: {
+        location: {
             lat: 40.7484405,
             lng: -73.9878584
         },
@@ -22,7 +22,7 @@ const DUMMY_PLACES = [
         description: 'One of the most famous skycrapers in the world',
         imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
         address: '20 W 34th St, New York, NY 10001, USA',
-        latitude: {
+        location: {
             lat: 40.7484405,
             lng: -73.9878584
         },
