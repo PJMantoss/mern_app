2422fa0cb4f05683613127d91fa69e4bc73f0bc2
PJMantoss <joelptoss@gmail.com>
2021-08-20 15:36:12 +0100

    update Input

--- frontend/src/shared/components/FormElements/Input.js
+++ frontend/src/shared/components/FormElements/Input.js
@@ -3,6 +3,10 @@ import React from "react";
 import './Input.css';
 
 const Input = props => {
+    const element = props.element === 'input' ? (
+        <input id={props.id} type={props.type} placeholder={props.placeholder} />
+        ) : (<textarea />);
+
     return(
         <div className={`form-control`}>
             <label htmlFor={props.id}>{props.label}</label>
