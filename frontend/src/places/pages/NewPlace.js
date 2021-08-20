1ea327f366e90ce5ff76e904ad4a03f22beaadb5
PJMantoss <joelptoss@gmail.com>
2021-08-20 15:40:10 +0100

    update NewPlace

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Input from '../../shared/components/FormElements/Input';
+
+import './NewPlace.css'
 
 const NewPlace = () => {
     return <form className="place-form">
