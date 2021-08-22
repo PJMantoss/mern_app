f523dc3478ee69e2be922eb16375d228fc60ca53
PJMantoss <joelptoss@gmail.com>
2021-08-22 06:53:57 +0100

    update NewPlace

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -21,8 +21,8 @@ const NewPlace = () => {
             id="description"
             element="textarea"
             label="description" 
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a valid title."
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid description (at least 5 characters)."
             onInput={titleInputHandler}
         />
     </form>
