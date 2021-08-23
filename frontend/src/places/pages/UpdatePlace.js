0c7805e4cdd7ae9094a088b19fb841c4ae5ce116
PJMantoss <joelptoss@gmail.com>
2021-08-23 09:34:16 +0100

    update App.js

--- frontend/src/places/pages/UpdatePlace.js
+++ frontend/src/places/pages/UpdatePlace.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: "Empire State Building",
+        description: 'One of the most famous skycrapers in the world',
+        imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
+        address: '20 W 34th St, New York, NY 10001, USA',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u1'
+    },
+
+    {
+        id: 'p2',
+        title: "Empire State Building",
+        description: 'One of the most famous skycrapers in the world',
+        imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
+        address: '20 W 34th St, New York, NY 10001, USA',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u2'
+    }
+]
+
 const UpdatePlace = () => {
     const placeId = useParams().placeId;
+
+    const identifiedPlace = DUMMY_PLACES.find(p => p.id === id)
     return(<h2>Update Place</h2>)
 };
 
