dbd194f5ab1c3c22f8c41e49acf8e0bfe36e9107
PJMantoss <joelptoss@gmail.com>
2021-08-23 09:30:29 +0100

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
 
 function App() {
   return( 
@@ -21,6 +22,10 @@ function App() {
 
         <Route path="/places/new" exact>
           <NewPlace />
+        </Route>
+
+        <Route path="/places/:placeId" exact>
+          <UpdatePlace />
         </Route>
 
         <Redirect to="/" exact />
