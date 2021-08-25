7317bf17b7e451a3c86bd9f5420017d87ade2b55
PJMantoss <joelptoss@gmail.com>
2021-08-25 12:42:21 +0100

    rename NewPlace.css to PlaceForm.css

--- frontend/src/places/pages/UpdatePlace.js
+++ frontend/src/places/pages/UpdatePlace.js
@@ -5,6 +5,8 @@ import Button from '../../shared/components/FormElements/Button';
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 
+import './PlaceForm.css'
+
 const DUMMY_PLACES = [
     {
         id: 'p1',
