c5ce1f7916d91812d3bff54382f29dd192470e9f
PJMantoss <joelptoss@gmail.com>
2021-09-23 12:33:26 +0100

    update Auth page

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Input from '../../shared/components/FormElements/Input';
 
 function Auth(props) {
     return (
         <form>
-            
+            <Input 
+                id="name"
+                element="input" 
+                type="text" 
+                label="Name" 
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter your name."
+                onInput={inputHandler}
+            />
         </form>
     );
 }
