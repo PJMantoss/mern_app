f23f11e38e6994f8ab51439ffb5a7cb325e94bed
PJMantoss <joelptoss@gmail.com>
2021-09-23 08:55:29 +0100

    update form-hook

--- frontend/src/shared/hooks/form-hook.js
+++ frontend/src/shared/hooks/form-hook.js
@@ -50,7 +50,7 @@ export const useForm = (initialInputs, initialValidity) => {
             inputs: inputData,
             formIsValid: formValidity
         })
-    })
+    }, []);
 
     return [formState, inputHandler, setFormData]
 };
