44b2106e03b737342035d4cdb93d7da31a47d2bc
PJMantoss <joelptoss@gmail.com>
2021-09-23 09:26:52 +0100

    update PlaceList

--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -11,7 +11,7 @@ const PlaceList = props => {
         <div className="place-list center">
             <Card>
                 <h2>No Places Found. Maybe Create One?</h2>
-                <button>Share Place</button>
+                <Button to="/places">Share Place</Button to="/places">
             </Card>
         </div>);
     }
