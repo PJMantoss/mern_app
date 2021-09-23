a173ef1d477e6a3ff0ed7fddc4d016a299333201
PJMantoss <joelptoss@gmail.com>
2021-09-23 08:59:43 +0100

    import useEffect in UpdatePlace

--- frontend/src/places/pages/UpdatePlace.js
+++ frontend/src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
