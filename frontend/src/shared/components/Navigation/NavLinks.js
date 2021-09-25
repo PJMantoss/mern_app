71e631fcf9906266c0d01eb824d7952052059ef0
PJMantoss <joelptoss@gmail.com>
2021-09-25 18:02:11 +0100

    update NavLinks

--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -6,13 +6,14 @@ import './NavLinks.css';
 
 const NavLinks = props => {
     const auth = useContext(AuthContext);
-    
+
     return(
         <ul className="nav-links">
             <li>
                 <NavLink to="/" exact>ALL USERS</NavLink>
             </li>
 
+            {auth.isLoggedIn && ()}
             <li>
                 <NavLink to="/u1/places">MY PLACES</NavLink>
             </li>
