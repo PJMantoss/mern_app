4e35a762f36d431bcbc9db2fdc9d2e257bc37e94
PJMantoss <joelptoss@gmail.com>
2021-09-25 17:29:59 +0100

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ function App() {
   }, []);
 
   return( 
-    <AuthContext.Provider>
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
       <Router>
         <MainNavigation />
         <main>
