d20241b9eef590c4c1b3d94909d4ab1462580a40
PJMantoss <joelptoss@gmail.com>
2021-09-25 17:16:25 +0100

    update auth-context

--- frontend/src/shared/context/auth-context.js
+++ frontend/src/shared/context/auth-context.js
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
-export const AuthConext = createContext({
+export const AuthContext = createContext({
     isLoggedIn: false,
     login: () => {},
     logout: () => {}
