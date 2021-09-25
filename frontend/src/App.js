86f52c6b2aa3d187e587d50546cff958b7b6aa3f
PJMantoss <joelptoss@gmail.com>
2021-09-25 18:28:35 +0100

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -63,27 +63,7 @@ function App() {
         <MainNavigation />
         <main>
           <Switch>
-            <Route path="/" exact>
-              <Users />
-            </Route>
-
-            <Route path="/:userId/places">
-              <UserPlaces />
-            </Route>
-
-            <Route path="/places/new" exact>
-              <NewPlace />
-            </Route>
-
-            <Route path="/places/:placeId">
-              <UpdatePlace />
-            </Route>
-
-            <Route path="/auth">
-              <Auth />
-            </Route>
-
-            <Redirect to="/" exact />
+            {routes}
           </Switch>
         </main>
       </Router>
