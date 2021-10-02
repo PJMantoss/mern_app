c876e0296e6bc2ff76e35cd1996219cc3d1eba91
PJMantoss <joelptoss@gmail.com>
2021-10-02 16:45:50 +0100

    update places-routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -25,7 +25,9 @@ router.get('/:pid', (req, res, next) => {
     });
 
     if(!place){
-        return res.status(404).json({message: "Could not find a place for the provided id."});
+        const error = new Error("Could not find a place for the provided id.");
+        error.code = 404;
+        throw error;
     }
     
     res.json({ place });
