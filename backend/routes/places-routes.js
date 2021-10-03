40b77198298de042a76829219a59a375d2213f63
PJMantoss <joelptoss@gmail.com>
2021-10-03 16:05:29 +0100

    update places-routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -4,21 +4,6 @@ const router = express.Router();
 
 const httpError = require('../models/http-error');
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: "Empire State Building",
-        description: 'One of the most famous skycrapers in the world',
-        imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
-        address: '20 W 34th St, New York, NY 10001, USA',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u1'
-    }
-]
-
 router.get('/:pid', (req, res, next) => {
     const placeId = req.params.pid;
 
