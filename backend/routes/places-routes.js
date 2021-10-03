80e06b30989086a539d41a369d90b569618c84d6
PJMantoss <joelptoss@gmail.com>
2021-10-03 16:15:52 +0100

    update places-routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const placesControllers = require('../controllers/places-controller');
 
-router.get('/:pid');
+router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid');
 
