381dd7c2829b94dd09a51c216d2f4dd42877964b
PJMantoss <joelptoss@gmail.com>
2021-10-03 16:08:28 +0100

    update places-routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -6,20 +6,6 @@ const httpError = require('../models/http-error');
 
 router.get('/:pid');
 
-router.get('/user/:uid', (req, res, next) => {
-    const userId = req.params.uid;
-
-    const place = DUMMY_PLACES.find(p => {
-        return p.creator === userId;
-    });
-
-    if(!place){
-        return next(
-            new Error("Could not find a place for the provided user id.", 404)
-        );
-    }
-
-    res.json({ place })
-});
+router.get('/user/:uid');
 
 module.exports = router;
