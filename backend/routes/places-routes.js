4551bf06807db9162cf8bbc0be441c1c58c0271d
PJMantoss <joelptoss@gmail.com>
2021-10-04 14:34:39 +0100

    update places-routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -10,8 +10,8 @@ router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
 router.post('/', placesControllers.createPlace);
 
-router.patch('/', placesControllers.updatePlace);
+router.patch('/:pid', placesControllers.updatePlace);
 
-router.delete('/', placesControllers.deletePlace);
+router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports = router;
