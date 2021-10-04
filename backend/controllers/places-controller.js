94cb25b230bf66dbe4ab1854080faf3769946ec8
PJMantoss <joelptoss@gmail.com>
2021-10-04 16:01:14 +0100

    update places-controller

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -89,7 +89,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
