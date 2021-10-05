4f8f1b939229802539286f314e50efe327f08708
PJMantoss <joelptoss@gmail.com>
2021-10-05 09:53:39 +0100

    update users-controllers

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -2,23 +2,17 @@ const httpError = require('../models/http-error');
 
 const {v4: uuidv4} = require('uuid');
 
-let DUMMY_PLACES = [
+let DUMMY_USERS = [
     {
-        id: 'p1',
-        title: "Empire State Building",
-        description: 'One of the most famous skycrapers in the world',
-        imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
-        address: '20 W 34th St, New York, NY 10001, USA',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u1'
+        id: 'u1',
+        name: "PJ Toss",
+        email: 'pj@example.com',
+        password: 'passing'
     }
 ]
 
 const getUsers = (req, res, next) => {
-    res.status(200).json({ message: "All Users" })
+    res.json({ users: DUMMY_USERS })
 };
 
 const signup = (req, res, next) => {
