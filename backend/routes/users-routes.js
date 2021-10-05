c9af812e27c044d4ec3122fb473d97dcbca21210
PJMantoss <joelptoss@gmail.com>
2021-10-05 15:58:06 +0100

    update users-routes

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 
+const { check } = require('express-validator');
+
 const router = express.Router();
 
 const usersController = require('../controllers/users-controller');
 
 router.get('/', usersController.getUsers);
 
-router.post('/signup', usersController.signup);
+router.post(
+    '/signup', 
+    check('name')
+    .not()
+    .isEmpty(),
+    usersController.signup
+    );
 
 router.post('/login', usersController.login);
 
