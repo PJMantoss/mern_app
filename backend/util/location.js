98b05d62a88dd90f5e10690491bcf366b59c19bd
PJMantoss <joelptoss@gmail.com>
2021-10-05 19:04:47 +0100

    update location.js

--- backend/util/location.js
+++ backend/util/location.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const httpError = require('../models/http-error');
 
 API_KEY = 'AIzaSyAs1ttmZ1WP1iJbtpwFoPHEYvhBXN7K7mA';
 
@@ -11,4 +12,9 @@ async function getCoordsForAddress(address){
     const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
 
     const data = response.data
+
+    if(!data || data.status === 'ZERO_RESULTS'){
+        const error = new httpError('Could not find location for the specified address', 422); //422 - status code for invalid user input
+        throw error;
+    }
 }
