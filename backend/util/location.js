6eaa3ef4a55c4c90f06bb31f0366e5205e740f2b
PJMantoss <joelptoss@gmail.com>
2021-10-05 18:48:51 +0100

    install axios

--- backend/util/location.js
+++ backend/util/location.js
@@ -1,8 +1,10 @@
+const axios = require('axios');
+
 API_KEY = 'AIzaSyAs1ttmZ1WP1iJbtpwFoPHEYvhBXN7K7mA';
 
-async function getCoordsForAddress(address){
-    return {
-        lat: 40.7484405,
-        lng: -73.9878584
-    }
-}
+// async function getCoordsForAddress(address){
+//     return {
+//         lat: 40.7484405,
+//         lng: -73.9878584
+//     }
+// }
