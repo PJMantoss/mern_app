068ec1e4c2a029482238810f30fb4fa17f5c5cbb
PJMantoss <joelptoss@gmail.com>
2021-10-05 09:14:50 +0100

    update users-routes

---
+++ backend/controllers/users-controller.js
@@ -0,0 +1,18 @@
+const httpError = require('../models/http-error');
+
+const {v4: uuidv4} = require('uuid');
+
+let DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: "Empire State Building",
+        description: 'One of the most famous skycrapers in the world',
+        imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
+        address: '20 W 34th St, New York, NY 10001, USA',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u1'
+    }
+]
