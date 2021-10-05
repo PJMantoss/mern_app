9e517310ffb558c71c20e227c82dd0d0d1f42c39
PJMantoss <joelptoss@gmail.com>
2021-10-05 18:51:14 +0100

    update location.js

--- backend/util/location.js
+++ backend/util/location.js
@@ -2,9 +2,11 @@ const axios = require('axios');
 
 API_KEY = 'AIzaSyAs1ttmZ1WP1iJbtpwFoPHEYvhBXN7K7mA';
 
-// async function getCoordsForAddress(address){
-//     return {
-//         lat: 40.7484405,
-//         lng: -73.9878584
-//     }
-// }
+async function getCoordsForAddress(address){
+    // return {
+    //     lat: 40.7484405,
+    //     lng: -73.9878584
+    // }
+
+    axios.get(``);
+}
