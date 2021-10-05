a8934467c2f01ce325849c9d1f0283bebbe418ab
PJMantoss <joelptoss@gmail.com>
2021-10-05 13:34:00 +0100

    update places-routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -15,8 +15,10 @@ router.post(
     check('title')
     .not()
     .isEmpty(), 
-    check('description')
-    .isLength(), 
+    check('description').isLength({ min: 5 }),
+    check('address')
+    .not()
+    .isEmpty(), 
     placesControllers.createPlace
     );
 
