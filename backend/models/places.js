8c7df4a5a9788ff1e9976ca66f27db91e0a9023d
PJMantoss <joelptoss@gmail.com>
2021-10-09 21:31:49 +0100

    update places schema

--- backend/models/places.js
+++ backend/models/places.js
@@ -2,4 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const placesSchema = new Schema({})
+const placesSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true }
+});
