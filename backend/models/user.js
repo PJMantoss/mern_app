f711544a8349e316273493a35b20805a73c57921
PJMantoss <joelptoss@gmail.com>
2021-10-14 11:06:28 +0100

    update user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const uniqueValidator = require('mongoose-unique-validator');
+
 const userSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
