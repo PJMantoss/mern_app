9153845c9636953633a75b244c054a974303ba1e
PJMantoss <joelptoss@gmail.com>
2021-10-18 17:29:39 +0100

    import ErrorModal and LoadingSpinner in Auth.js

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -5,6 +5,8 @@ import { useForm } from '../../shared/hooks/form-hook';
 import Button from '../../shared/components/FormElements/Button';
 import Card from '../../shared/components/UIElements/Card';
 import { AuthContext } from '../../shared/context/auth-context';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 
 import './Auth.css'
 
