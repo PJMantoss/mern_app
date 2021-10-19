3a3f5fb656822cfe0033b085fd7ac5673b117c6b
PJMantoss <joelptoss@gmail.com>
2021-10-19 22:33:42 +0100

    update Users.js

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -9,11 +9,13 @@ const Users = () => {
     useEffect(() => {
         const sendRequest = async () => {
             setIsLoading(true);
-            
+
             try{
                 const response = await fetch('http://localhost:5000/api/users');
 
                 const responseData = response.json();
+
+                setLoadedUsers(responseData.users);
 
                 setIsLoading(false);
             }catch(err){
