9adc5cc241fc9b3f0b9d9085d4db12e59e6e902f
PJMantoss <joelptoss@gmail.com>
2021-10-19 22:19:46 +0100

    update Users.js

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS = [
-    {
-        id: 'u1',
-        name: 'Snoop Dog',
-        image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
-        places: 3
-    }
-]
+
+//     const USERS = [
+//     {
+//         id: 'u1',
+//         name: 'Snoop Dog',
+//         image: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+//         places: 3
+//     }
+// ]
 
     return <UsersList users={USERS} />
 };
