d61ac2cb0c0ca21128396e71b7ab8994cbaf43b6
PJMantoss <joelptoss@gmail.com>
2021-10-20 21:11:18 +0100

    update Users.js

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -15,7 +15,7 @@ const Users = () => {
             try{
                 const response = await fetch('http://localhost:5000/api/users');
 
-                const responseData = response.json();
+                const responseData = await response.json();
 
                 if(!response.ok){
                     throw new Error(responseData.message);
