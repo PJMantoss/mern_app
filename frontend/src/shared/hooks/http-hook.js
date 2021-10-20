543d31196caad949082280f52a82d7e417f5cfc6
PJMantoss <joelptoss@gmail.com>
2021-10-20 22:15:24 +0100

    update http-hook

--- frontend/src/shared/hooks/http-hook.js
+++ frontend/src/shared/hooks/http-hook.js
@@ -1,24 +1,29 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 const useHttpClient = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState();
 
-    const sendRequest = async (url, method = 'GET', body = 'null', headers = {}) => {
-        try{
-            const response = await fetch(url, {
-                method,
-                body,
-                headers
-            });
-    
-            const responseData = await response.json();
-                     
-            if(!response.ok){
-                throw new Error(responseData.message);
+    const sendRequest = useCallback(
+        async (url, method = 'GET', body = 'null', headers = {}) => {
+            setIsLoading(true);
+            try{
+                const response = await fetch(url, {
+                    method,
+                    body,
+                    headers
+                });
+        
+                const responseData = await response.json();
+                         
+                if(!response.ok){
+                    throw new Error(responseData.message);
+                }
+            }catch(err){
+                setError(err.message);
             }
-        }catch(err){
-            setError(err.message);
-        }
-    }
+    
+            setIsLoading(false);
+        }, []
+    )
 };
