f84fc2c989e1291eab27041b304405304f774bce
PJMantoss <joelptoss@gmail.com>
2021-10-20 22:10:23 +0100

    update http-hook

--- frontend/src/shared/hooks/http-hook.js
+++ frontend/src/shared/hooks/http-hook.js
@@ -5,16 +5,20 @@ const useHttpClient = () => {
     const [error, setError] = useState();
 
     const sendRequest = async (url, method = 'GET', body = 'null', headers = {}) => {
-        const response = await fetch(url, {
-            method,
-            body,
-            headers
-        });
-
-        const responseData = await response.json();
-                 
-        if(!response.ok){
-            throw new Error(responseData.message);
+        try{
+            const response = await fetch(url, {
+                method,
+                body,
+                headers
+            });
+    
+            const responseData = await response.json();
+                     
+            if(!response.ok){
+                throw new Error(responseData.message);
+            }
+        }catch(err){
+            setError(err.message);
         }
     }
 };
