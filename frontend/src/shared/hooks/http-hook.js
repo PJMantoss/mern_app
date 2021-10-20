6ff9a71d9ddd870649ae3205b640b56b8ff1b4b0
PJMantoss <joelptoss@gmail.com>
2021-10-20 22:04:01 +0100

    update http-hook

--- frontend/src/shared/hooks/http-hook.js
+++ frontend/src/shared/hooks/http-hook.js
@@ -3,4 +3,10 @@ import { useState } from 'react';
 const useHttpClient = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState();
+
+    const sendRequest = (url, method = 'GET', body = 'null', headers = {}) => {
+        fetch(url, {
+            method
+        })
+    }
 };
