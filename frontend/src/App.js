91e1968d65bb57c111a7f334bfd3653d99ce8a54
PJMantoss <joelptoss@gmail.com>
2021-10-23 22:25:48 +0100

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,13 +11,16 @@ import { AuthContext } from './shared/context/auth-context';
 function App() {
   //Login state
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(false);
 
-  const login = useCallback(() => {
+  const login = useCallback((uid) => {
     setIsLoggedIn(true);
+    setUserId(uid);
   }, []);
 
   const logout = useCallback(() => {
     setIsLoggedIn(false);
+    setUserId(null);
   }, []);
 
   let routes;
