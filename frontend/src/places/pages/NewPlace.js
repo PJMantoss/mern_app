820293445fe871ac3da741bb1aa5520f6be94553
PJMantoss <joelptoss@gmail.com>
2021-10-24 12:06:41 +0100

    update NewPlace.js

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from '../../shared/context/auth-context';
 
 import './PlaceForm.css'
 
 const NewPlace = () => {
+    const auth = useContext(AuthContext);
 
     const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
@@ -38,7 +40,7 @@ const NewPlace = () => {
                 title: formState.inputs.title.value,
                 description: formState.inputs.description.value,
                 address: formState.inputs.address.value,
-                creator: 
+                creator: auth.userId
             })
         ); //Send these info to the backend
     }
