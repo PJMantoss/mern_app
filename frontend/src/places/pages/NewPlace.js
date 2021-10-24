51a09ac729a7476eb44a956d72005ebe8f5485c9
PJMantoss <joelptoss@gmail.com>
2021-10-24 12:13:08 +0100

    update NewPlace.js

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -5,6 +5,8 @@ import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/valida
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpClient } from '../../shared/hooks/http-hook';
 import { AuthContext } from '../../shared/context/auth-context';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 import './PlaceForm.css'
 
@@ -49,36 +51,40 @@ const NewPlace = () => {
 
     }
 
-    return <form className="place-form" onSubmit={placeSubmitHandler}>
-        <Input 
-            id="title"
-            element="input" 
-            type="text" 
-            label="Title" 
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a valid title."
-            onInput={inputHandler}
-        />
-        <Input 
-            id="description"
-            element="textarea"
-            label="Description" 
-            validators={[VALIDATOR_MINLENGTH(5)]}
-            errorText="Please enter a valid description (at least 5 characters)."
-            onInput={inputHandler}
-        />
-        <Input 
-            id="address"
-            element="input"
-            label="Address" 
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a valid address."
-            onInput={inputHandler}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-            ADD PLACE
-        </Button>
-    </form>
+    return (
+        <React.Fragment>
+            <form className="place-form" onSubmit={placeSubmitHandler}>
+                <Input 
+                    id="title"
+                    element="input" 
+                    type="text" 
+                    label="Title" 
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please enter a valid title."
+                    onInput={inputHandler}
+                />
+                <Input 
+                    id="description"
+                    element="textarea"
+                    label="Description" 
+                    validators={[VALIDATOR_MINLENGTH(5)]}
+                    errorText="Please enter a valid description (at least 5 characters)."
+                    onInput={inputHandler}
+                />
+                <Input 
+                    id="address"
+                    element="input"
+                    label="Address" 
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please enter a valid address."
+                    onInput={inputHandler}
+                />
+                <Button type="submit" disabled={!formState.isValid}>
+                    ADD PLACE
+                </Button>
+            </form>
+        </React.Fragment>
+    )
 }
 
 export default NewPlace;
