18a3452bc23969045ce23da66ef5b9e7d7ecd70c
PJMantoss <joelptoss@gmail.com>
2021-10-25 22:45:56 +0100

    update UserPlaces

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,36 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PlaceList from '../components/PlaceList';
 import { useParams } from 'react-router-dom';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: "Empire State Building",
-        description: 'One of the most famous skycrapers in the world',
-        imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
-        address: '20 W 34th St, New York, NY 10001, USA',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u1'
-    },
+// const DUMMY_PLACES = [
+//     {
+//         id: 'p1',
+//         title: "Empire State Building",
+//         description: 'One of the most famous skycrapers in the world',
+//         imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
+//         address: '20 W 34th St, New York, NY 10001, USA',
+//         location: {
+//             lat: 40.7484405,
+//             lng: -73.9878584
+//         },
+//         creator: 'u1'
+//     },
 
-    {
-        id: 'p2',
-        title: "Empire State Building",
-        description: 'One of the most famous skycrapers in the world',
-        imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
-        address: '20 W 34th St, New York, NY 10001, USA',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u2'
-    }
-]
+//     {
+//         id: 'p2',
+//         title: "Empire State Building",
+//         description: 'One of the most famous skycrapers in the world',
+//         imageUrl: 'https://images.unsplash.com/photo-1538708027031-6955608d3c26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80',
+//         address: '20 W 34th St, New York, NY 10001, USA',
+//         location: {
+//             lat: 40.7484405,
+//             lng: -73.9878584
+//         },
+//         creator: 'u2'
+//     }
+// ]
 
 const UserPlaces = () => {
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    
     const userId = useParams().userId;
     const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
     return <PlaceList items={loadedPlaces} />
