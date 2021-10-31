c1dcf9ecbf03bab61b31ac1a51d59dbc644b0b90
PJMantoss <joelptoss@gmail.com>
2021-10-31 16:59:36 +0100

    update userPlaces

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PlaceList from '../components/PlaceList';
 import { useParams } from 'react-router-dom';
 import { useHttpClient } from '../../shared/hooks/http-hook';
@@ -32,13 +32,25 @@ import { useHttpClient } from '../../shared/hooks/http-hook';
 // ]
 
 const UserPlaces = () => {
+    const [loadedPlaces, setLoadedPlaces] = useState();
     const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
     const userId = useParams().userId;
 
-    useEffect(() => {}, []);
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            try{
+                const responseData = await sendRequest(
+                    `http://localhost:5000/api/places/user/${userId}`
+                );
+                setLoadedPlaces(responseData.places)
+            }catch(err){}
+        };
 
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+        fetchPlaces();
+    }, [sendRequest]);
+
+    //const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
     
     return <PlaceList items={loadedPlaces} />
 } 
