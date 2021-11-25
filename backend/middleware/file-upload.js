4f3a19775db35cd0582e20bf5bf9fa882f0f8422
PJMantoss <joelptoss@gmail.com>
2021-11-25 18:00:12 +0100

    update fileUpload

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -1,10 +1,16 @@
 const multer = require('multer');
+const {v4: uuidv4} = require('uuid');
 
-const MIME_TYPE_MAP = {};
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpeg'
+};
 
 const fileUpload = multer({
     limits: 500000,
-    storage: multer.diskStorage({});
+    storage: multer.diskStorage({}),
+    filename: (req, file, cb) => {}
 });
 
 module.exports = fileUpload;
