f027cfc5ecb36ebe667b86c2ce6344ae8fd7a490
PJMantoss <joelptoss@gmail.com>
2021-11-25 15:40:23 +0100

    add ImageUpload.js

--- frontend/src/shared/components/FormElements/ImageUpload.js
+++ frontend/src/shared/components/FormElements/ImageUpload.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import Button from './Button';
 
 import './ImageUpload.css';
 
 const ImageUpload = props => {
     return (
-        <div>
+        <div className="form-control">
             <input 
                 id={props.id} 
                 type="file"
-                style={{ display: none }}
+                style={{ display: 'none' }}
                 accept=".jpg,.png,.jpeg" 
             />
+            <div className={`image-upload ${props.center && 'center'}`}></div>
         </div>
     )
 };
