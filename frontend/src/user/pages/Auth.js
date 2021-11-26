bb48632f599d4ca8d25c8493f3d4d65505b874d0
PJMantoss <joelptoss@gmail.com>
2021-11-26 16:36:10 +0100

    update Auth

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -65,44 +65,42 @@ const Auth = () => {
         if(isLoginMode){       
 
               try{
-                  const formData = new FormData();
-                  formData.append('email', formState.inputs.email.value);
-                  formData.append('name', formState.inputs.name.value);
-                  formData.append('password', formState.inputs.password.value);
-                  formData.append('image', formState.inputs.image.value);
                 const responseData = await sendRequest(
                     'http://localhost:5000/api/users/login', 
                     'POST', 
-                    formData
-                //     JSON.stringify({
-                //     email: formState.inputs.email.value,
-                //     password: formState.inputs.password.value
-                // }),
-                // {
-                //     'Content-Type': 'application/json'
-                // }
-              ); //HTTP Request to the backend
-
-              auth.login(responseData.user.id);
-
-              }catch(err){
-
-              };
-             
-        }else{
-
-            try{
-                const responseData = await sendRequest(
-                    'http://localhost:5000/api/users/signup', 
-                    'POST', 
                     JSON.stringify({
-                    name: formState.inputs.name.value,
                     email: formState.inputs.email.value,
                     password: formState.inputs.password.value
                 }),
                 {
                     'Content-Type': 'application/json'
                 }
+              ); //HTTP Request to the backend
+
+              auth.login(responseData.user.id);
+
+              }catch(err){};
+             
+        }else{
+
+            try{
+                  const formData = new FormData();
+                  formData.append('email', formState.inputs.email.value);
+                  formData.append('name', formState.inputs.name.value);
+                  formData.append('password', formState.inputs.password.value);
+                  formData.append('image', formState.inputs.image.value);
+                  const responseData = await sendRequest(
+                    'http://localhost:5000/api/users/signup', 
+                    'POST', 
+                    formData
+                //     JSON.stringify({
+                //     name: formState.inputs.name.value,
+                //     email: formState.inputs.email.value,
+                //     password: formState.inputs.password.value
+                // }),
+                // {
+                //     'Content-Type': 'application/json'
+                // }
               ); //HTTP Request to the backend
                 
                 auth.login(responseData.user.id);
