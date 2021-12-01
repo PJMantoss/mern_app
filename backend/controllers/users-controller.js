76ddcb07e0f3ca2739fa1369dcb894778afd9f5a
PJMantoss <joelptoss@gmail.com>
2021-12-01 23:45:39 +0100

    update users-controller

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -110,7 +110,7 @@ const login = async (req, res, next) => {
         return next(error);
     }
 
-    if(!existingUser || existingUser.password !== password){
+    if(!existingUser){
         const error = new httpError('Invalid credentials, could not login', 401);
         return next(error);
     }
