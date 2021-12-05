d82588342f5438ba20e47d9eb75dd9d547e1947e
PJMantoss <joelptoss@gmail.com>
2021-12-05 22:04:27 +0100

    update check-auth

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,3 +1,13 @@
+const httpError = require("../models/http-error");
+
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization.split(' '); // Authorization: 'Bearer TOKEN'
+    try{
+        const token = req.headers.authorization.split(' '); // Authorization: 'Bearer TOKEN'
+        if(!token){
+            throw new Error('Authentication failed');
+        }
+    }catch(err){
+        const error = new httpError('Authentication failed', 401);
+        return next(error);
+    }
 };
