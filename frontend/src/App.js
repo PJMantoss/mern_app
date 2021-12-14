493b2c80acd86b0a3dd9e4fba6c1dd7539a2f1a8
PJMantoss <joelptoss@gmail.com>
2021-12-14 17:19:24 +0100

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,19 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 function App() {
   //Login state
   const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false);
 
   const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
-    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60)
+    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
     localStorage.setItem(
       'userData', 
       JSON.stringify({ 
@@ -32,6 +36,12 @@ function App() {
     setUserId(null);
     localStorage.removeItem('userData');
   }, []);
+
+  useEffect(() => {
+    if(token){
+      setTimeout()
+    }
+  },[token, logout, tokenExpirationDate])
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem('userData'));
