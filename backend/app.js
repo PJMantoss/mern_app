7fd1f0d3727e69c1507ec46185d47264981c9a54
PJMantoss <joelptoss@gmail.com>
2021-12-18 17:28:22 +0100

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -57,7 +57,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-    .connect('mongodb+srv://mantoss:superstar@cluster0.kxgzl.mongodb.net/mern?retryWrites=true&w=majority')
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kxgzl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     .then(() => {
         app.listen(5000); //If connection was sucessful start backend server
     })
